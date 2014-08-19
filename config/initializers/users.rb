3c798688ca7ea071f62b0f79e63d8b7259917e55
Liothen <liothen@flagrun.net>
2014-08-19 16:44:37 -0700

    Created owner role

--- config/initializers/users.rb
+++ config/initializers/users.rb
@@ -1,3 +1,4 @@
 # Load Users
+$owner             = Zconf.roles.owner
 $admin             = Zconf.roles.admin.split(' ') || Array.new
 $operator          = Zconf.roles.operator.split(' ') || Array.new
