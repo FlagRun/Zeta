fa6cbaa921a37711f9ae84f018b6f4103fbe4d6b
Liothen <liothen@flagrun.net>
2014-08-21 14:31:03 -0700

    Updated urban plugin to use unofficial api

--- plugins/urban.rb
+++ plugins/urban.rb
@@ -7,8 +7,9 @@ module Plugins
     include Cinch::Plugin
     set(
         plugin_name: 'UrbanDictionary',
-        help: "Urban Dictionary -- Grabs a term from urbandictionary.com.\nUsage: `!urban <term>`; `!wotd`; `!woty`"
+        help: "Urban Dictionary -- Grabs a term from urbandictionary.com.\nUsage: `?urban <term>`; `?wotd`; `!?woty`"
     )
+
     match /urban (.*)/, method: :query
     match /ud (.*)/,    method: :query
     match /wotd/,       method: :wotd
@@ -23,8 +24,8 @@ module Plugins
     def woty(m)
       url = URI.encode "http://www.urbandictionary.com/woty.php"
       doc = Nokogiri.HTML(open url)
-      word = doc.at_css('.word').text.strip[0..800]
-      meaning = doc.at_css('.meaning').text.strip[0..800]
+      word = doc.at_css('.word').text.strip[0..500]
+      meaning = doc.at_css('.meaning').text.strip[0..500]
       m.reply "UD> #{word} -- #{meaning}"
     end
 
@@ -32,21 +33,31 @@ module Plugins
 
       url = URI.encode "http://www.urbandictionary.com/"
       doc = Nokogiri.HTML(open url)
-      word = doc.at_css('.word').text.strip[0..800]
-      meaning = doc.at_css('.meaning').text.strip[0..800]
+      word = doc.at_css('.word').text.strip[0..500]
+      meaning = doc.at_css('.meaning').text.strip[0..500]
       m.reply "UD> #{word} -- #{meaning}"
     end
 
     private
     def search(query)
-      url = URI.encode "http://www.urbandictionary.com/define.php?term=#{query}"
-      Nokogiri.HTML(open url).at_css('.meaning').text.strip[0..800]
+      # url = URI.encode "http://www.urbandictionary.com/define.php?term=#{query}"
+      # Nokogiri.HTML(open url).at_css('.meaning').text.strip[0..500]
+
+      # Load API data
+      url = JSON.parse(
+          open("http://api.urbandictionary.com/v0/define?term=#{query}").read
+      )
+
+      # Return if nothing is found
+      return 'No Results found' if url['result_type'] == 'no_results'
+
+      # Return first definition
+      url['list'].first['definition'].strip[0..500]
     rescue => e
       e.message
     end
   end
 end
-
 
 # AutoLoad
 Zeta.config.plugins.plugins.push Plugins::UrbanDictionary
