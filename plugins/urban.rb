643464fe036599df38ac3d92bb901a8033160b81
Liothen <liothen@flagrun.net>
2014-08-21 14:44:40 -0700

    Removed WOTY fixed url encodeing

--- plugins/urban.rb
+++ plugins/urban.rb
@@ -17,20 +17,11 @@ module Plugins
 
 
     def query(m, query)
-
       m.reply "UD> #{search(query)}"
     end
 
-    def woty(m)
-      url = URI.encode "http://www.urbandictionary.com/woty.php"
-      doc = Nokogiri.HTML(open url)
-      word = doc.at_css('.word').text.strip[0..500]
-      meaning = doc.at_css('.meaning').text.strip[0..500]
-      m.reply "UD> #{word} -- #{meaning}"
-    end
 
     def wotd(m)
-
       url = URI.encode "http://www.urbandictionary.com/"
       doc = Nokogiri.HTML(open url)
       word = doc.at_css('.word').text.strip[0..500]
@@ -40,19 +31,19 @@ module Plugins
 
     private
     def search(query)
-      # url = URI.encode "http://www.urbandictionary.com/define.php?term=#{query}"
+      url = URI.encode "http://api.urbandictionary.com/v0/define?term=#{query}"
       # Nokogiri.HTML(open url).at_css('.meaning').text.strip[0..500]
 
       # Load API data
-      url = JSON.parse(
-          open("http://api.urbandictionary.com/v0/define?term=#{query}").read
+      data = JSON.parse(
+          open(url).read
       )
 
       # Return if nothing is found
-      return 'No Results found' if url['result_type'] == 'no_results'
+      return 'No Results found' if data['result_type'] == 'no_results'
 
       # Return first definition
-      url['list'].first['definition'].strip[0..500]
+      data['list'].first['definition'].strip[0..500]
     rescue => e
       e.message
     end
