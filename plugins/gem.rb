f0fdc2960cc60119d2d8098e98949c1228078cd0
Liothen <liothen@flagrun.net>
2014-10-17 19:35:11 -0700

    Disable some old autoloader gems

--- plugins/gem.rb
+++ plugins/gem.rb
@@ -1,13 +1,8 @@
-require 'cinch/plugins/quotes'
-# require 'cinch-convert'
 require 'cinch-calculate'
 require 'weather-underground'
 
 Zeta.config.plugins.plugins   << Cinch::Plugins::Calculate
-# Zeta.config.plugins.plugins   << Cinch::Plugins::Convert
-# Zeta.config.plugins.plugins   << Cinch::Plugins::Quotes
+
 
 Zeta.config.plugins.options[Cinch::Plugins::Calculate][:units_path]   = '/usr/bin/units'
-# Zeta.config.plugins.options[Cinch::Plugins::Convert][:units_path]     = '/usr/bin/units'
-# Zeta.config.plugins.options[Cinch::Plugins::Quotes][:quotes_file]     = $root_path + '/data/quotes.yml'
 
