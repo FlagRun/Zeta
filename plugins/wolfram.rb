f099fbac530276e1cc21cb51d085b74f0ce2e832
Liothen <liothen@flagrun.net>
2014-12-04 17:19:42 -0800

    wolfram alpha plugin... thats in alpha shape

---
+++ plugins/wolfram.rb
@@ -0,0 +1,33 @@
+require 'crack'
+
+module Plugins
+  class Wolfram
+    include Cinch::Plugin
+    include Cinch::Helpers
+    include ActionView::Helpers::DateHelper
+
+
+    self.plugin_name = 'Wolfram Alpha plugin'
+    self.help = 'WIP'
+
+    match /wolfram (.+)/, method: :calculate
+    match /wolframalpha (.+)/, method: :calculate
+    match /calc (.+)/, method: :calculate
+
+    def calculate(msg, query)
+      return unless check_user(msg)
+      return unless check_channel(msg)
+
+      url = URI.encode "http://api.wolframalpha.com/v2/query?input=#{query}&appid=#{Zsec.wolfram}&primary=true&format=plaintext"
+      request = RestClient.get(url)
+      data = Crack::XML.parse(request)
+      answer = data['queryresult']['pod'][1]['subpod']['plaintext']
+      msg.user.notice "# Wolfram Results #\n #{answer}"
+
+    end
+
+  end
+end
+
+# AutoLoad
+Zeta.config.plugins.plugins.push Plugins::Wolfram
