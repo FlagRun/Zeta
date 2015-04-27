eaf494a5e87883f5b4ac87d6a581da64beece3a7
Jan Harasym <dijit@drk.sc>
2015-04-27 14:42:14 +0200

    Update gif.rb
    [*] Implemented reddit scraper for imgur.
    [*] changed namespace for gifbin (random is too ambiguous)
    
    Caching support can be added easily by wrapping 'imgur' in a if statement until `imgurray.sample`
    
    Q: does 'imgur' get executed on invokation (IE: just once when the program is launched)? if it does then we need to revisit how this works.

--- plugins/gif.rb
+++ plugins/gif.rb
@@ -1,5 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
+require 'json'
 
 module Plugins
   class Gif
@@ -14,7 +15,14 @@ module Plugins
     def randomgif(msg)
       return unless check_user(msg)
       return unless check_channel(msg)
-      msg.reply "GB↦ #{random}" if random != nil
+      msg.reply "GB↦ #{gifbin}" if gifbin != nil
+    end
+
+    match "imgif", method: :imgif
+    def imgif(msg)
+        return unless check_user(msg)
+        return unless check_channel(msg)
+        msg.reply "IMGUR↦ #{imgur}" if imgur != nil
     end
 
     match /gifme (.+)/, method: :gifme
@@ -28,19 +36,31 @@ module Plugins
 
     def search(query)
       url = URI.encode "http://www.gifbin.com/search/#{query}/"
-      doc = Nokogiri::HTML( RestClient.get(url) )
+      doc = Nokogiri::HTML( open(url) )
       e = doc.css('.thumbs li').length
       return "No Results Found" if e == 0
       result = doc.css('.thumbs li')[rand(e)].css('a img').attribute('src').text.gsub(/tn_/, '')
       "http://www.gifbin.com#{result}"
     end
 
-    def random
+    def gifbin
       url = URI.encode 'http://www.gifbin.com/random'
-      doc = Nokogiri.HTML(RestClient.get(url))
+      doc = Nokogiri.HTML(open url)
       doc.css('div#gifcontainer a img').attribute('src').text
     end
 
+    def imgur
+        # TODO: add caching support
+        url = URI.encode('http://reddit.com/r/gifs.json')
+        doc = JSON.load(open(url))
+        imgurray = []
+        doc['data']['children'].each_with_index do |post,index|
+          if doc['data']['children'][index]['data']['url'].to_s =~ /imgur/
+             imgurray << doc['data']['children'][index]['data']['url'].to_s
+          end
+        end
+      imgurray.sample
+    end
   end
 end
 
