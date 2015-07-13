3b8fdcedd9fa987e0080312e0468dfb15ec0a8cf
Liothen <liothen@flagrun.net>
2015-07-13 15:00:25 -0700

    Meme plugin
    This plugin was added to store specific meme’s that the community uses.

---
+++ plugins/meme.rb
@@ -0,0 +1,57 @@
+module Plugins
+  class Meme
+    include Cinch::Plugin
+    include Cinch::Helpers
+
+    set(
+        plugin_name: "MeMe",
+        help:        "Display a meme?.\nUsage: `?m <meme>`",
+    )
+
+    # Regex
+    match /m (.+)/, method: :find_meme
+
+
+    # Initialization
+    def initialize(*args)
+      @meme = load_locale('meme')
+      super
+    end
+
+    # Methods
+    def find_meme(m, query)
+      query.strip!
+
+      return unless check_user(m)
+      return unless check_channel(m)
+      return help_meme(m) if query == 'help'
+      return reload_meme(m) if query == 'reload'
+      return m.reply('Unknown meme - Use ?m help') unless @meme.has_key?(query)
+
+      m.reply "M → #{@meme[query].sample}"
+    end
+
+    # Private
+    private
+    def help_meme(m)
+      m.reply "Currently accepted meme's are #{@meme.keys.join(',')}."
+    end
+
+    def reload_meme(m)
+      return unless check_user(m, :admin)
+      # return unless check_channel(m)
+      begin
+        # @macros = YAML::load_file($root_path + '/locales/macros.yml')
+        @meme = load_locale 'meme'
+        m.user.notice "Meme's have been reloaded."
+      rescue
+        m.user.notice "Reloading macros has failed: #{$!}"
+      end
+    end
+
+  end
+end
+
+
+# AutoLoad
+Zeta.config.plugins.plugins.push Plugins::Meme
