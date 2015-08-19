52ab9a8c3f5b7aeaf7a14e657c88b07c865f72ae
Liothen <liothen@flagrun.net>
2015-08-19 13:02:16 -0700

    updated check_user to ACL

--- lib/helpers/acl.rb
+++ lib/helpers/acl.rb
@@ -30,68 +30,95 @@ module Cinch
 
 
     def check_user(m, role=:nobody)
-      u = find_user(m)
+      u = update_user(m)
+      return false unless u
+      c = u.plugin_usage.to_i + 1
+      Zuser.where(id: u.id).update(plugin_usage: c)
       return false if u.ignore
       return true if u.access.to_i >= LEVELS[role]
       u
     end
 
     def find_user(m)
-      u = update_user(m)
-      return false unless u
-      c = u.plugin_usage.to_i + 1
-      Zuser.where(id: u.id).update(plugin_usage: c)
-      return u
-    end
+      # Determine the proper object
+      if m.class == Cinch::User
+        user = m
+      elsif m.class == Cinch::Message
+        user = m.user
+      else
+        return false
+      end
 
-    def update_user(m)
-      if defined?(m.user.authname) && m.user.authname
-        u = Zuser.find(authname: m.user.authname)
+      # Determine if the user has an authname
+      if defined?(user.authname) && user.authname
+        u = Zuser.find(authname: user.authname)
         if u
-          Zuser.where(authname: m.user.authname).update(
-              nickname: m.user.nick,
-              hostname: m.user.host,
-              username: m.user.user
+          Zuser.where(authname: user.authname).update(
+              nickname: user.nick,
+              hostname: user.host,
+              username: user.user
           )
           return u
         else
           u = Zuser.create(
-              nickname: m.user.nick,
-              username: m.user.user,
-              hostname: m.user.host,
-              authname: m.user.authname
+              nickname: user.nick,
+              username: user.user,
+              hostname: user.host,
+              authname: user.authname
           )
           return u
         end
       else
-        return false unless defined?(m.user.host) #just incase we create a user with User('test')
-        u = Zuser.find(nickname: m.user.nick, hostname: m.user.host)
-        unless u
+        return false unless defined?(user.host) #just incase we create a user with User('test')
+        u = Zuser.find(nickname: user.nick, hostname: user.host)
+        if u
+          return u
+        else
           u = Zuser.create(
-              nickname: m.user.nick,
-              username: m.user.user,
-              hostname: m.user.host
+              nickname: user.nick,
+              username: user.user,
+              hostname: user.host
           )
           return u
         end
       end
+
+    end
+
+    def update_user(m)
+      find_user(m)
     end
 
     def check_channel(m)
-      c = update_channel(m)
+      c = find_channel(m)
+      return true unless defined?(c.disabled)
       return false if c.disabled == 1
       c
     end
 
-    def update_channel(m)
-      if m.channel.name
-        c = Zchannel.find(name: m.channel.name)
+    def find_channel(m)
+      if m.class == Cinch::Channel
+        channel = m
+      elsif m.class == Cinch::Message
+        channel = m.channel
+      else
+        return false
+      end
+
+      if defined?(channel.name) && channel.name
+        c = Zchannel.find(name: channel.name)
         unless c
-          c = Zchannel.create(name: m.channel.name)
+          c = Zchannel.create(name: channel.name)
           return c
         end
+      else
+        c = true
       end
       c
+    end
+
+    def update_channel(m)
+      find_channel(m)
     end
 
   end
