7a91981267f85d355f7f9d4d4f3a627c93dd370b
Liothen <liothen@flagrun.net>
2019-12-15 11:53:58 -0800

    Rename ?whois to ?noticeme
    Signed-off-by: Liothen <liothen@flagrun.net>

--- lib/Zeta/plugins/noticeme.rb
+++ lib/Zeta/plugins/noticeme.rb
@@ -1,18 +1,18 @@
 module Plugins
-  class Whois
+  class Noticeme
     include Cinch::Plugin
     include Cinch::Helpers
 
     # enable_acl
 
-    self.plugin_name = 'Whois'
-    self.help        = 'Resync your user'
+    self.plugin_name = 'Notice ME!'
+    self.help        = 'Make sure zeta can see me!'
 
     # Regex
-    match 'whois', method: :do_whois
+    match 'noticeme', method: :notice_me
 
     # Methods
-    def do_whois(m)
+    def notice_me(m)
       m.user.refresh
       m.user.notice "Refreshing #{m.user.nick}"
     end
@@ -22,4 +22,4 @@ module Plugins
 end
 
 # AutoLoad
-Bot.config.plugins.plugins.push Plugins::Whois
+Bot.config.plugins.plugins.push Plugins::Noticeme
